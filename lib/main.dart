17db48520aa9fd475a83db366f808b65c1eb787e
wangyongqi <wyq@addcn.com>
2024-01-18 14:19:27 +0800

    Change counter

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,11 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_counter < 20) {
+        _counter++;
+      } else {
+        _counter = 0;
+      }
     });
   }
 
@@ -111,6 +115,9 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              '(min = 0, max = 20)',
             ),
           ],
         ),
